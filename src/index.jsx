3c3532b3c0494dd329297a76f1a08555697157b2
Lyuboslav Petkov <140117647+LyuboPetkov@users.noreply.github.com>
2024-12-26 21:24:14 +0200

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Use these commands to run:
+// npm install
+// npm run dev
